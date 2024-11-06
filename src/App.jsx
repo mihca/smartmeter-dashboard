a717d978be1ce7057e8c2663100518a8a58a9c0f
mihca <am@amsoftware.de>
2024-11-06 20:25:02 +0100

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-red-500 text-4xl bg-blue-500">
+        It's
+      </div>
+    </>
+  )
+}
+
+export default App
