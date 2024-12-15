5e842000ade7d8c488b981758e8cd66a80e8a23e
mihca <am@amsoftware.de>
2024-12-15 15:24:14 +0100

    Context path

--- src/tables/TariffsTable.jsx
+++ src/tables/TariffsTable.jsx
@@ -20,6 +20,7 @@ export default function TariffsTable ({usagePDR, marketData, onBestTariffFound})
 	const [selectedNetfees, setSelectedNetfees] = useState("0");
 	const [vatChecked, setVatChecked] = useState(true);
 	const [basefeeChecked, setBasefeeChecked] = useState(true);
+	const [showCtPerKwh, setCtPerKwh] = useState(true);
 	
 	function handleNetfeesChange (e) {
 		setSelectedNetfees(e.target.value);
@@ -256,11 +257,15 @@ export default function TariffsTable ({usagePDR, marketData, onBestTariffFound})
 							>
 								<td className='px-2 py-2 whitespace-nowrap text-sm font-medium'>
 									<p className='text-gray-100'>{lineData.date}</p>
-									<p className='text-gray-500'>{lineData.averageMarketPricePerKwh.toFixed(1)}</p>
+									{ showCtPerKwh && (
+										<p className='text-gray-500'>{lineData.averageMarketPricePerKwh.toFixed(1)}</p>
+									)}
 								</td>
 								<td className='px-2 py-2 whitespace-nowrap text-sm font-medium'>
 									<p className={(idx === array.length-1) ? 'text-gray-100':'text-gray-100'}>{lineData.kwh.toFixed(2)} kWh</p>
-									<p className='text-gray-500'>{lineData.weightedMarketPricePerKwh.toFixed(1)}</p>
+									{ showCtPerKwh && (
+										<p className='text-gray-500'>{lineData.weightedMarketPricePerKwh.toFixed(1)}</p>
+									)}
 								</td>
 								{ lineData.tariffPricesEUR.map ( (priceEUR, idxTariff, pricesArray) => (
 								<td className='px-2 py-2 whitespace-nowrap text-sm'
@@ -268,7 +273,9 @@ export default function TariffsTable ({usagePDR, marketData, onBestTariffFound})
 									<Bill bill={lineData.priceInfo[idxTariff]}>
 										<p className={highlightBestPrice(priceEUR, pricesArray, idx === array.length-1)}>{priceEUR.toFixed(2)} EUR</p>
 									</Bill>
-									<p className='text-gray-500'>{(priceEUR/lineData.kwh*100).toFixed(1)}</p>
+									{ showCtPerKwh && (
+										<p className='text-gray-500'>{(priceEUR/lineData.kwh*100).toFixed(1)}</p>
+									)}
 								</td>
 								))}
 							</motion.tr>
