4de016581ce569d656d05c35ba8a581950641e07
Achim Müller <achim.mueller@ebcont.com>
2025-01-16 20:09:29 +0100

    Storage simulator monthly

--- src/features/storage-simulator/simulate-storage.js
+++ src/features/storage-simulator/simulate-storage.js
@@ -4,75 +4,107 @@ import { calculateHour, calculateNetfee, addVat } from "../tariff-calculator/cal
 export function simulateStorage (usagePDR, feedinPDR, mdr, selectedStorageSize, selectedChargingLoss, selectedUsageTariff, selectedNetfees, selectedFeedinTariff) {
     const lineData = [];
 
-    if (usagePDR && feedinPDR) {
+    if (!usagePDR || !feedinPDR) 
+        return lineData;
         
-        const usageHourData = usagePDR.hourData;
-        const feedinHourData = feedinPDR.hourData;
-        const capacity = Number(selectedStorageSize);
-        const ladeverluste = Number(selectedChargingLoss);
-        let soc = 0.0;
-        let kohle_eur = 0.0;
-        
-        usageHourData.forEach((usageHourEntry, idx, array) => {
+    const usageHourData = usagePDR.hourData;
+    const feedinHourData = feedinPDR.hourData;
+    const capacity = Number(selectedStorageSize);
+    const ladeverluste = Number(selectedChargingLoss);
+     
+    let groupId = 0;
+    let groupChange = (date, groupId) => date.getMonth() != groupId;
+    let groupCounter = 0;
+    let lineDatePattern = "yyyy-MM";
+    let sumKwhUsage = 0.0;
+    let sumKwhFeedin = 0.0;
+    let sumKwhCharged = 0.0;
+    let sumKwhDischarged = 0.0;
+    let sumSoc = 0.0;
+    let sumEurProfit = 0.0;
 
-            const date = new Date (usageHourEntry.utcHour);
-            const kwh_verbrauch = usageHourEntry.kwh;
-            const kwh_einspeisung = feedinHourData[idx].kwh;
-            const marketPrice = mdr.hourMap.get(usageHourEntry.utcHour-3600000)
+    let soc = 0.0;
+    let gewinn_eur = 0.0;
 
-            // Akku laden
-            let kwh_ladung = 0;
+    usageHourData.forEach((usageHourEntry, idx, array) => {
 
-            if (kwh_einspeisung > 0) {
+        const date = new Date (usageHourEntry.utcHour);
+        const kwh_verbrauch = usageHourEntry.kwh;
+        const kwh_einspeisung = feedinHourData[idx].kwh;
+        const marketPrice = mdr.hourMap.get(usageHourEntry.utcHour-3600000)
+    
+        // Akku laden
+        let kwh_ladung = 0;
 
-                let kwh_ladung_netto = kwh_einspeisung * (1 - ladeverluste / 100);
-                if (soc + kwh_ladung_netto > capacity) {
-                    // Akku voll
-                    kwh_ladung = (capacity - soc) * (1 + ladeverluste / 100);
-                    soc = capacity;
-                } else {
-                    // Komplette Einspeisung in den Akku geladen
-                    soc = soc + kwh_ladung_netto;
-                    kwh_ladung = kwh_einspeisung;
-                }
+        if (kwh_einspeisung > 0) {
 
-                const kohle_nicht_erhalten = calculateHour (selectedFeedinTariff, kwh_ladung, usageHourEntry.utcHour, marketPrice);
-                kohle_eur = kohle_eur - kohle_nicht_erhalten
-                // print("Time: %s, Akku geladen mit %f kWh, Soc: %f, Geld verloren: %f ct" % (point_verbrauch['time'], kwh_einspeisung_netto, soc, kohle_nicht_erhalten))            }
+            let kwh_ladung_netto = kwh_einspeisung * (1 - ladeverluste / 100);
+            if (soc + kwh_ladung_netto > capacity) {
+                // Akku voll
+                kwh_ladung = (capacity - soc) * (1 + ladeverluste / 100);
+                soc = capacity;
+            } else {
+                // Komplette Einspeisung in den Akku geladen
+                soc = soc + kwh_ladung_netto;
+                kwh_ladung = kwh_einspeisung;
             }
 
-            // Akku entladen
-            let kwh_verbrauch_neu = kwh_verbrauch;
-            let kwh_gespart = 0;
+            const eur_nicht_erhalten = calculateHour (selectedFeedinTariff, kwh_ladung, usageHourEntry.utcHour, marketPrice);
+            gewinn_eur = gewinn_eur - eur_nicht_erhalten
+        }
 
-            if (kwh_verbrauch > 0 && soc > 0) {
-                soc = soc - kwh_verbrauch;
-                kwh_verbrauch_neu = 0;
-                if (soc < 0) {
-                    kwh_verbrauch_neu = -soc;
-                    soc = 0;
-                }
-                kwh_gespart = kwh_verbrauch - kwh_verbrauch_neu;
-                let kohle_gespart = calculateHour (selectedUsageTariff, kwh_gespart, usageHourEntry.utcHour, marketPrice);
-                // We need 1 our and not 1 day
-                kohle_gespart += calculateNetfee (selectedNetfees, 0, kwh_gespart);
-                kohle_gespart += addVat (kohle_gespart);
-                kohle_eur = kohle_eur + kohle_gespart;
-                // print("Time: %s, Akku entladen mit %f kWh, Soc: %f, Geld gespart: %f ct" % (point_verbrauch['time'], kwh_gespart, soc, kohle_gespart))
+        // Akku entladen
+        let kwh_verbrauch_neu = kwh_verbrauch;
+        let kwh_gespart = 0;
+
+        if (kwh_verbrauch > 0 && soc > 0) {
+            soc = soc - kwh_verbrauch;
+            kwh_verbrauch_neu = 0;
+            if (soc < 0) {
+                kwh_verbrauch_neu = -soc;
+                soc = 0;
             }
+            kwh_gespart = kwh_verbrauch - kwh_verbrauch_neu;
+            let eur_gespart = calculateHour (selectedUsageTariff, kwh_gespart, usageHourEntry.utcHour, marketPrice);
+            eur_gespart += calculateNetfee (selectedNetfees, 0, kwh_gespart);
+            eur_gespart += addVat (eur_gespart);
+            gewinn_eur += eur_gespart;
+        }
+
+        sumKwhUsage += kwh_verbrauch;
+        sumKwhFeedin += kwh_einspeisung;
+        sumKwhCharged += kwh_ladung;
+        sumKwhDischarged += kwh_gespart;
+        sumEurProfit += gewinn_eur;
+        sumSoc += soc;
+        groupCounter += 1;
+
+        // Change of day or month
+        if (groupChange(new Date(usageHourEntry.utcHour), groupId) || (idx === array.length - 1)) {
+
+            const endDate = new Date(array[idx-1].utcHour);
 
             lineData.push({
-                date: format(date, "yyyy-MM-dd HH:mm"),
-                usedKwh: kwh_verbrauch,
-                feedinKwh: kwh_einspeisung,
-                chargedKwh: kwh_ladung,
-                dischargedKwh: kwh_gespart,
-                socKwh: soc,
-                socPercent: soc / selectedStorageSize,
-                eurSaved: kohle_eur
+                date: format(endDate, lineDatePattern),
+                usedKwh: sumKwhUsage,
+                feedinKwh: sumKwhFeedin,
+                chargedKwh: sumKwhCharged,
+                dischargedKwh: sumKwhDischarged,
+                socKwh: sumSoc/groupCounter,
+                socPercent: sumSoc/groupCounter/selectedStorageSize,
+                eurSaved: gewinn_eur
             });
-        })
-    }
+
+            sumKwhUsage = 0.0;
+            sumKwhFeedin = 0.0;
+            sumKwhCharged = 0.0;
+            sumKwhDischarged = 0.0;
+            sumEurProfit = 0.0;
+            sumSoc = 0.0;
+            groupCounter = 0;
+            groupId += 1;        
+        }
+    })
 
     return lineData;
 }
