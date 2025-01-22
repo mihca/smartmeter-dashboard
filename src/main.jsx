bd8e6990ae7bc0c3039d2b174b90065bef997aaa
mihca <am@amsoftware.de>
2025-01-22 14:17:20 +0100

    Fix path

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NextUIProvider>
       <main className="dark text-foreground bg-background">
-        <BrowserRouter basename='smartmeter-dashboard'>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <App />
         </BrowserRouter>
       </main>
