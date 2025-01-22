50d456d8d7fe08cefbbeae63131ecc14afa0c478
mihca <am@amsoftware.de>
2025-01-22 08:45:19 +0100

    Fix context path

--- src/components/FileUploader.jsx
+++ src/components/FileUploader.jsx
@@ -24,7 +24,7 @@ export default function FileUploader({title, importError, onFileUploaded, demoFi
   };
 
   function handleDemoUpload() {
-    fetch(`./${demoFile}`)
+    fetch(`${demoFile}`)
       .then(response => response.text())
       .then(data => {
         setFilename(demoFile);
