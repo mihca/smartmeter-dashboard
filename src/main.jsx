e1b4be1bffca3752ecc28ab8df2d86b1856eed41
Achim Müller <achim.mueller@ebcont.com>
2025-01-23 09:44:53 +0100

    Switch to hash router for gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom"
-import {HeroUIProvider} from "@heroui/react"
+import { HashRouter } from "react-router-dom"
+import { HeroUIProvider } from "@heroui/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HeroUIProvider>
       <main className="dark text-foreground bg-background">
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </main>
     </HeroUIProvider>  
   </StrictMode>,
