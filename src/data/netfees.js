d6f03cbdb4e47111d9eef821ded00443e81ab03d
Achim Müller <achim.mueller@ebcont.com>
2025-01-29 08:47:43 +0100

    NetzNÖ 2025 added

--- src/data/netfees.js
+++ src/data/netfees.js
@@ -11,5 +11,16 @@ export const NETFEES = [
         netfee_per_kwh_ct: 6.553,
         // Elektrizitätsabgabe pro kWh in ct: 0.10
         tax_per_kwh_ct: 0.10
+    },
+    {
+        name: 'NetzNÖ 2025',
+        // Netznutzung Grundpreis pro Tag in ct: 9.863
+        // Messpreis pro Tag in ct: 7.1671
+        netfee_per_day_ct: 17.0301,
+        // Netznutzung Arbeitspreis pro kWh in ct: 10.02
+        // Netzverlustentgelt pro kWh in ct: 0.783
+        netfee_per_kwh_ct: 10.803,
+        // Elektrizitätsabgabe pro kWh in ct: 0.10
+        tax_per_kwh_ct: 0.10
     }
 ];
